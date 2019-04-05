c047ac3eb37b7b4d85a93cad1ac2cc6a86b411f6
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-04-05 17:19:06 -0400

    Initial Commit

---
+++ EstruturaDeDados/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  EstruturaDeDados
+//
+//  Created by Eliza Maria Porto de Carvalho on 05/04/19.
+//  Copyright © 2019 Academy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
