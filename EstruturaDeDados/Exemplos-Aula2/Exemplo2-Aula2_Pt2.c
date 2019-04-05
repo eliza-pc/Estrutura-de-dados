5e368932605a04e56f54ee1a90a48aa9fe1924c6
Eliza Carvalho <elizampcarvalho@gmail.com>
2019-04-05 17:45:38 -0400

    Adiciona Exemplo2-Aula2_Pt2

---
+++ EstruturaDeDados/Exemplos-Aula2/Exemplo2-Aula2_Pt2.c
@@ -0,0 +1,17 @@
+//
+//  Exemplo2-Aula2_Pt2.c
+//  EstruturaDeDados
+//
+//  Created by Eliza Maria Porto de Carvalho on 05/04/19.
+//  Copyright © 2019 Academy. All rights reserved.
+//
+
+int soma_dobro(int a,int b)
+{
+    int soma;
+    a=2*a;
+    b=2*b;
+    soma=a+b;
+    
+    return 0;
+}
